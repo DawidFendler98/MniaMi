1e6ae79205c37bd22bdfcfd162b2bee0315705d9
Dawid <dawid_fendler98@wp.pl>
2021-04-12 16:59:36 +0200

    Add Navigation Component

--- app/src/androidTest/java/com/example/mniami/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/mniami/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.recipes
+package com.example.mniami
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
--- app/src/test/java/com/example/mniami/ExampleUnitTest.kt
+++ app/src/test/java/com/example/mniami/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.recipes
+package com.example.mniami
 
 import org.junit.Test
 
